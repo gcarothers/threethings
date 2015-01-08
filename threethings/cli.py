d8ab9d9c2274e8118cc1145500dc3e88115f4e06
Gavin Carothers <gavin@carothers.name>
2015-01-07 20:44:34 -0800

    Use a config file
    Drop per command db_url, prefer per command config

--- threethings/cli.py
+++ threethings/cli.py
@@ -1,8 +1,11 @@
 """Command line interface for threethings"""
-
+import os.path
 import argh
+import json
 from argh import (
     arg,
+    confirm,
+    safe_input,
 )
 from .model import (
     Session,
@@ -20,6 +23,7 @@ import pytz
 import transaction
 
 DEFAULT_DATABASE_URL='postgresql://threethings@127.0.0.1:5432/threethings-dev'
+DEFAULT_CONFIG_PATH='~/.config/3things.json'
 
 
 def _setup_database(db_url):
@@ -29,15 +33,48 @@ def _setup_database(db_url):
     Base.metadata.bind = engine
 
 
-def create_schema(db_url=DEFAULT_DATABASE_URL):
-    _setup_database(db_url)
+def _setup_from_config(config_path):
+    from sqlalchemy import engine_from_config
+    config = _load_config(config_path)
+    engine = engine_from_config(config['database'], prefix='')
+    Session.configure(bind=engine)
+    Base.metadata.bind = engine
+
+
+def _load_config(config_path):
+    path = os.path.expanduser(config_path)
+    with open(path) as config_file:
+        config = json.load(config_file)
+    return config
+
+def _write_config(config, config_path):
+    path = os.path.expanduser(config_path)
+    with open(path, mode='w') as config_file:
+        json.dump(config, config_file)
+
+
+def create_schema(config=DEFAULT_CONFIG_PATH):
+    _setup_from_config(config)
     Base.metadata.create_all()
 
 
+def config(path=DEFAULT_CONFIG_PATH):
+    database_url = safe_input("Database URL [{}]: ".format(DEFAULT_DATABASE_URL))
+    if database_url is "":
+        database_url = DEFAULT_DATABASE_URL
+
+    config = {
+        'database': {
+            'url': database_url,
+        },
+    }
+    _write_config(config,
+                  config_path=path)
+
 def add_user(email_address,
              timezone='America/Los_Angeles',
-             db_url=DEFAULT_DATABASE_URL):
-    _setup_database(db_url)
+             config=DEFAULT_CONFIG_PATH):
+    _setup_from_config(config)
     with transaction.manager:
         user = User()
         user.email_address = email_address
@@ -48,8 +85,8 @@ def add_user(email_address,
 
 
 def remove_user(email_address,
-                db_url=DEFAULT_DATABASE_URL):
-    _setup_database(db_url)
+                config=DEFAULT_CONFIG_PATH):
+    _setup_from_config(config)
     with transaction.manager:
         user = Session.query(User).get(email_address)
         if user:
@@ -62,8 +99,8 @@ def remove_user(email_address,
 
 def send_reminders(date_override=None,
                    timezone="UTC",
-                   db_url=DEFAULT_DATABASE_URL):
-    _setup_database(db_url)
+                   config=DEFAULT_CONFIG_PATH):
+    _setup_from_config(config)
     if date_override is not None:
         when = parse(date_override)
         if when.tzinfo is None:
@@ -81,6 +118,7 @@ def send_reminders(date_override=None,
 parser = argh.ArghParser()
 parser.add_commands([
     add_user,
+    config,
     remove_user,
     create_schema,
     send_reminders,
