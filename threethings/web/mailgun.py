f1566705f06357ef0b2aaddb091244b412d901ce
Shawn Eisenach <wholewheattoast@gmail.com>
2016-05-12 17:10:36 -0700

    In progress work wiring up mailgun webhook. Have webhook working, but issues remain sending response.

--- threethings/web/mailgun.py
+++ threethings/web/mailgun.py
@@ -25,7 +25,7 @@ log = logging.getLogger(__name__)
 
 def includeme(config):
     config.add_route('mailgun_landing', '/')
-    config.add_route('mailgun_receiving', '/receive2')
+    config.add_route('mailgun_receiving', '/receive')
     config.scan()
 
 
@@ -38,29 +38,37 @@ def webhook_allowed(request):
              request_method='POST',
              renderer='json')
 def receive_email(request):
-    mailgun_events = request.params['mailgun_events']
-    email_json = json.loads(mailgun_events)
+    mailgun_events = {}
+    for i in request.params:
+        try:
+            mailgun_events[i] = request.params[i]
+        except TypeError, e:
+            log.error("------ {}".format(e))
+            pass
+        except:
+            pass
+    mailgun_events_json = json.dumps(mailgun_events)
+    email_json = json.loads(mailgun_events_json)
     mailer = get_mailer(request)
     updates = process_inbound_email(mailer, email_json)
     return list(updates)
 
 
 def process_inbound_email(mailer, email_json):
-    """Based on http://goo.gl/vyeI69 process the incoming email."""
-    for event in email_json:
-        if event['event'] != 'inbound':
-            raise ValueError('Unexpected event: {}'.format(event['event']))
-        timestamp = datetime.datetime.fromtimestamp(event['ts'], tz=pytz.UTC)
-        msg = event['msg']
-        author = msg['from_email']
-        text = msg['text']
-        html = msg['html']
-        subject = msg['subject']
-        message_id = msg['headers'].get('Message-Id')
-        update = StatusUpdate.from_email(author, timestamp, text, html)
-        send_confirm(mailer,
-                     update.user,
-                     reply_to_id=message_id,
-                     reply_to_subject=subject,
-                     )
-        yield update
+    log.info("------ doing a thing for {}".format(email_json['from']))
+    timestamp = datetime.datetime.fromtimestamp(
+        float(email_json['timestamp']), tz=pytz.UTC
+    )
+    author = email_json['sender']
+    text = email_json['body-plain']
+    html = email_json['body-html']
+    subject = email_json['subject']
+    # this is typed as a unicode and cant index correctly?
+    # message_id = email_json['message-headers'].get('Message-Id')
+    update = StatusUpdate.from_email(author, timestamp, text, html)
+    send_confirm(mailer,
+                 update.user,
+                 #reply_to_id=reply_to,
+                 reply_to_subject=subject,
+                 )
+    yield update
